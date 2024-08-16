894deb2a37a87699dc75a5443036e28908d1fa49
Ali Burak KAYA <aliburakkayas@hotmail.com>
2024-08-16 12:27:11 +0300

    You get e-mail of your Transaction Report attached.

--- src/main/java/com/burak/safa_bank_app/service/impl/EmailService.java
+++ src/main/java/com/burak/safa_bank_app/service/impl/EmailService.java
@@ -4,4 +4,5 @@ import com.burak.safa_bank_app.dto.EmailDetails;
 
 public interface EmailService {
     void sendEmailAlert(EmailDetails emailDetails);
+    void sendEmailWithAttachment(EmailDetails emailDetails);
 }
